c301a6be38196ce7f5e09a828ac0170d9d2094b5
JingKangKk <juliaimwinter@126.com>
2025-05-01 18:57:53 +0200

    Update EDA.sql with total spending per customer query

--- queries/EDA.sql
+++ queries/EDA.sql
@@ -1,3 +1,9 @@
 SELECT column_name
 FROM information_schema.COLUMNS
 WHERE table_name = 'transaction_2'
+
+
+SELECT cust_id, SUM(amount) AS total_spent
+FROM transaction_2
+GROUP BY cust_id
+ORDER BY total_spent DESC;
